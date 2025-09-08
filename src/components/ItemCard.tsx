ba31c0e6a74c9d239ef9a1682aed0b4f6cc37e6e
TTjirakit <captain112006@gmail.com>
2025-09-08 11:41:55 +0700

    .

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,19 +1,39 @@
 import { Card, Group, Badge, ActionIcon, Text } from "@mantine/core";
 import { IconTrash } from "@tabler/icons-react";
 
-type ExpenseProps = {
-  name: string;
-  amount: number | string;
-  category: string;
-  onDelete: () => void;
-};
+// type ExpenseProps = {
+//   name: string;
+//   amount: number | string;
+//   category: string;
+//   onDelete: () => void;
+// };
 
 export default function ItemCard() {
   // หากต้องการเปลี่ยนแปลง type ชนิด string เป็น number สามารถใช้วิธีการดังโค้ดตัวอย่างด้านล่างนี้ได้
   let val_number: number = Number("500.0");
   console.log(val_number + 100); // 600.0
 
-  return {
-    /* Type additional text here. */
-  };
+  return (
+     <Card p="md">
+
+      <Group justify="space-between" mt="md" mb="xs">
+      <Text>
+          
+      </Text>
+
+      <Text>
+        
+      </Text>
+
+      <Badge color="blue" radius="md">
+        ExpenseProps.category
+      </Badge>
+     
+      <ActionIcon color="red">
+        <IconTrash/>
+      </ActionIcon>
+
+      </Group>
+    </Card>
+  );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import {
-  Modal,
-  TextInput,
-  NumberInput,
-  Select,
-  Button,
-  Stack,
-} from "@mantine/core";
+// import { useState } from "react";
+// import {
+//   Modal,
+//   TextInput,
+//   NumberInput,
+//   Select,
+//   Button,
+//   Stack,
+// } from "@mantine/core";
 
 type AddExpenseModalProps = {
   opened: boolean;
@@ -19,17 +19,17 @@ type AddExpenseModalProps = {
 };
 
 export default function AddExpenseModal({}: AddExpenseModalProps) {
-  const [name, setName] = useState<string>("");
-  const [amount, setAmount] = useState<string | number>(0);
-  const [category, setCategory] = useState<string | null>(null);
+  // const [name, setName] = useState<string>("");
+  // const [amount, setAmount] = useState<string | number>(0);
+  // const [category, setCategory] = useState<string | null>(null);
 
-  const handleSubmit = () => {};
+  // const handleSubmit = () => {};
 
   // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
   let val_number: number = Number("500.0");
   console.log(val_number + 100); // 600.0
 
   return {
-    /* Type additional text here. */
+    
   };
 }
